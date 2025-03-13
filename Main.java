64c71dbf93e0fb5fee0c58bee437a75b15421953
MOHAMMED AUSAF SHAIKH <58620927+ausafshaikhh@users.noreply.github.com>
2025-03-13 07:46:12 +0530

    added new line

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a java program");
         System.out.println("hello world");
+        System.out.println("added new line");
     }
 }
